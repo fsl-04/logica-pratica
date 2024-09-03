bc2c5aca5d64f7241c0e61442ca84f59ef5398d0
Carlos Vieira <112698218+carloeira@users.noreply.github.com>
2024-09-02 21:37:05 -0300

    incluindo mais exemplos

--- main.js
+++ main.js
@@ -1,12 +1,45 @@
-let precoOriginal = parseFloat(prompt("Digite o valor do produto: "));
-let percentualDoDesconto = parseFloat(prompt("Digite o percentual do desconto: "));
+// let precoOriginal = parseFloat(prompt("Digite o valor do produto: "));
+// let percentualDoDesconto = parseFloat(prompt("Digite o percentual do desconto: "));
 
-let valorDoDesconto = (precoOriginal * percentualDoDesconto) / 100;
-let precoFinal = precoOriginal - valorDoDesconto;
+// let valorDoDesconto = (precoOriginal * percentualDoDesconto) / 100;
+// let precoFinal = precoOriginal - valorDoDesconto;
 
-console.log("Valor do desconto: R$ " + valorDoDesconto.toFixed(2));
-console.log("Preço final: R$ " + precoFinal.toFixed(2));
+// console.log("Valor do desconto: R$ " + valorDoDesconto.toFixed(2));
+// console.log("Preço final: R$ " + precoFinal.toFixed(2));
 
-if (percentualDoDesconto > 50) {
-    console.log("Atenção! O desconto é muito alto!");
-}
+// if (percentualDoDesconto > 50) {
+//     console.log("Atenção! O desconto é muito alto!");
+// }
+
+// exemplo condicional
+
+// let idade = 15;
+
+// if (idade >= 18) {
+//     console.log("Você é maior de idade.");
+// } else {
+//     console.log("Você é menor de idade")
+// }
+
+// let dia = null;
+
+// switch (dia) { 
+//     case 1: console.log("Domingo"); 
+//     break; 
+//     case 2: 
+//     console.log("Segunda-feira"); 
+//     break; 
+//     case 3: console.log("Terça-feira"); 
+//     break; 
+//     default: console.log("Dia inválido"); 
+// }
+
+// for (let i = 0; i < 10; i++) {
+//     console.log(i);
+// }
+
+// let contador = 0;
+// while (contador < 10) {
+//     console.log(contador);
+//     contador++;
+// }
